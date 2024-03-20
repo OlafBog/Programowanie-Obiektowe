3eca497b9254fc482597729a7f289b9dbb9246d9
OlafBog <olafbogus04@gmail.com>
2024-03-20 15:28:47 +0100

    poprawki_2zajecia

--- src/Point.java
+++ src/Point.java
@@ -1,11 +1,17 @@
-public class Point
-{
-    public final int x;
-    public final int y;
+public class Point {
+    public final double x;
+    public final double y;
 
-    public Point(int x, int y)
-    {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,6 +1,28 @@
 public class Segment {
-    public toSvg()
-    {
+
+    private Point startPoint;
+    private Point endPoint;
+
+    public Segment(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    public Point getStartPoint() {
+        return startPoint;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    public double length() {
+        return Math.sqrt(Math.pow(endPoint.x - startPoint.x, 2) + Math.pow(endPoint.y - startPoint.y, 2));
 
     }
+
+    public String toSvg() {
+        return "<line x1=\"" + startPoint.x + "\" y1=\"" + startPoint.y + "\" x2=\"" + endPoint.x + "\" y2=\"" + endPoint.y + "\" stroke=\"black\" />"
+    }
+
 }
